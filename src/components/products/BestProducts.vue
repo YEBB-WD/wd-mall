cd9c7d6389b19680cb94630ff296cff9d6f5a627
David <juseok.yun@samsung.com>
2021-05-03 17:58:18 +0900

    update code

--- src/components/products/BestProducts.vue
+++ src/components/products/BestProducts.vue
@@ -1,45 +1,21 @@
 <template>
   <div class="best-products">
     <v-card>
-
+      <product-preview-list></product-preview-list>
     </v-card>
-    <div class="row">
-      <card-loader :loopCount="4" v-if="loading"/>
-      <div class="col-md-3" v-for="(item, index) in bestProducts" :key="index">
-        <v-row>
-          <v-col>
-            <product-preview></product-preview>
-          </v-col>
-        </v-row>
-      </div>
-      <div align="center" v-if="bestProducts.length == 0">
-        <v-row>
-          <v-col>
-            <product-preview></product-preview>
-          </v-col>
-          <v-col>
-            <product-preview></product-preview>
-          </v-col>
-          <v-col>
-            <product-preview></product-preview>
-          </v-col>
-          <v-col>
-            <product-preview></product-preview>
-          </v-col>
-        </v-row>
-      </div>
-    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import ProductPreview from "@/components/products/list/ProductPreview";
+import ProductPreviewList from "@/components/products/list/ProductPreviewList";
 
 export default {
   name: "BestProducts",
   components: {
      ProductPreview,
+     ProductPreviewList
   },
   data() {
     return {
---
+++ src/components/products/list/ProductPreviewList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div align="center">
+    <v-card max-width="96%">
+      <div class="row">
+        <div class="col-md-3" v-for="(item, index) in prdts" :key="index" v-if="prdts.length() > 0">
+          <product-preview></product-preview>
+        </div>
+      </div>
+    </v-card>
+  </div>
+</template>
+
+<script>
+
+import ProductPreview from "@/components/products/list/ProductPreview";
+
+export default {
+  name: "ProductPreviewList",
+  components: {
+    ProductPreview
+  },
+  props: {
+      prdts: {
+        types: Array,
+        default: [
+          {
+            prdt_img: ""
+          }
+        ]
+      }
+  },
+  data() { return {
+
+  }},
+}
+</script>
+
+<style scoped>
+
+</style>
